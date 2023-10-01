e6dd87ed1e8c561a04c80a05facb75a37f6b5307
Alex Polyansky <norton-f@list.ru>
2023-10-01 20:50:48 +0300

    Added dictionary, that provides separate indication object for all users

--- telega_SM.py
+++ telega_SM.py
@@ -1,25 +1,29 @@
+import os
+from datetime import datetime
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from telebot.types import CallbackQuery
 import openpyxl
-import os
-from datetime import datetime
+
 
 token = "5734991862:AAGVxppmtLjCTIMg6bN6TolKjgGl3wgdgSQ"
 
 my_bot = telebot.TeleBot(token)
 
+# словарь для размещения экземляров объектов indications 
+# (свой объект для каждого пользователя)
+users = {} 
+
 
 class indications():
-    def __init__(self, addr, appart, hot, cold, elct):
+    def __init__(self, addr, appart, hot, cold, elct, user_id):
         self.addr = addr
         self.appart = appart
         self.hot = hot
         self.cold = cold
         self.elct = elct
+        self.user_id = user_id
 
-
-current_indi = indications(addr="", appart="", hot="", cold="", elct="")
 
 # TELEBOT HANDLERS
 # start button
@@ -27,11 +31,11 @@ current_indi = indications(addr="", appart="", hot="", cold="", elct="")
 
 @my_bot.message_handler(commands=['start'])
 def welcome_message(message):
+    current_indi = indications(addr="", appart="", hot="", cold="", elct="", user_id=f'{message.from_user.first_name} {message.from_user.last_name} ID {message.from_user.id}')
     markup = address_keyboard(message, first_run=True)
-    my_bot.send_message(message.chat.id, text='Ващ айди ' +
-                        str(message.from_user.id))
-    my_bot.send_message(
-        message.chat.id, text='Выберете адрес:', reply_markup=markup)
+    my_bot.send_message(message.chat.id, text=f'User ID {message.chat.id}')
+    users[str(message.chat.id)] = current_indi
+    my_bot.send_message(message.chat.id, text='Выберете адрес:', reply_markup=markup)
 
 # text handler
 
@@ -52,7 +56,7 @@ def menu_navigate(callback_obj: CallbackQuery):
         stop_bot(message=callback_obj.message)
     elif len(callback_obj.data) > 4:  # кнопки адреса домов
         current_level = callback_obj.data[0]
-        current_indi.addr = callback_obj.data[2:]
+        users[str(callback_obj.message.chat.id)].addr = callback_obj.data[2:]
     else:
         current_level = callback_obj.data[0]  # нажата кнопка да
     levels = {
@@ -77,11 +81,11 @@ def make_rec(message, **kwargs):
         path = os.getcwd() + '\\'
     my_bot.send_message(message.chat.id, 'Пока!')
     wb = openpyxl.load_workbook(filename=path+'sample.xlsx')
-    ws = wb[current_indi.addr]
+    ws = wb[users[str(message.chat.id)].addr]
     time = str(datetime.now().strftime('%d.%m.%Y в %H:%M:%S'))
-    user = str(message.from_user.id)
-    ws.append([f'{current_indi.addr}, кв. {current_indi.appart}', f'{current_indi.hot}',
-              f'{current_indi.cold}', f'{current_indi.elct}', time, user])
+    user = users[str(message.chat.id)].user_id
+    ws.append([f'{users[str(message.chat.id)].addr}, кв. {users[str(message.chat.id)].appart}', f'{users[str(message.chat.id)].hot}',
+              f'{users[str(message.chat.id)].cold}', f'{users[str(message.chat.id)].elct}', time, user])
     wb.save(path+'sample.xlsx')
 
 @my_bot.message_handler(commands=['stop'])
@@ -115,19 +119,16 @@ def address_keyboard(message, first_run):
 
 def confirm_addr(message, current_level, **kwargs):
     list_of_check = [('Да'), ('Назад'), ('Выход')]
-    text = f'Верный адрес: {current_indi.addr}, кв. {current_indi.appart}?'
-    my_bot.send_message(chat_id=message.chat.id, text=text,
-                        reply_markup=gen_markup(list_of_check, 3, current_level+1))
+    text = f'Верный адрес: {users[str(message.chat.id)].addr}, кв. {users[str(message.chat.id)].appart}?'
+    my_bot.send_message(chat_id=message.chat.id, text=text, reply_markup=gen_markup(list_of_check, 3, current_level+1))
 
 # запрос на ввод номера квартиры
 
 
 def appart_input(message, **kwargs):
-    old_message = my_bot.edit_message_text(
-        chat_id=message.chat.id, message_id=message.id, text='Введите номер квартиры:')
+    old_message = my_bot.edit_message_text(chat_id=message.chat.id, message_id=message.id, text='Введите номер квартиры:')
     current_level = 1
-    my_bot.register_next_step_handler(
-        message, user_text_input, current_level, old_message)
+    my_bot.register_next_step_handler(message, user_text_input, current_level, old_message)
 
 # запрос на ввод показаний ГВ
 
@@ -165,27 +166,26 @@ def elct_keyboard(message, **kwargs):
 def confirm_keyboard(message, **kwargs):
     current_level = 5
     list_of_check = [('Да'), ('Назад'), ('Выход')]
-    text = f"""Передать показания для адреса {current_indi.addr}, кв. {current_indi.appart}: горячая вода - {current_indi.hot} куб., холодная вода - {current_indi.cold} куб., электричество - {current_indi.elct} кВт?"""
+    text = f"""Передать показания для адреса {users[str(message.chat.id)].addr}, кв. {users[str(message.chat.id)].appart}: горячая вода - {users[str(message.chat.id)].hot} куб., холодная вода - {users[str(message.chat.id)].cold} куб., электричество - {users[str(message.chat.id)].elct} кВт?"""
     my_bot.send_message(chat_id=message.chat.id, text=text,
                         reply_markup=gen_markup(list_of_check, 1, current_level+1))
 
-# обработчик пользовательского ввода
+# обработчик пользовательского ввода и внесение данных в объект indications
 
 
 def user_text_input(message, current_level, old_message):
-    my_bot.delete_message(chat_id=old_message.chat.id,
-                          message_id=old_message.id)
+    my_bot.delete_message(chat_id=old_message.chat.id, message_id=old_message.id)
     if current_level == 1:
-        current_indi.appart = message.text
+        users[str(message.chat.id)].appart = message.text
         confirm_addr(message, current_level)
     elif current_level == 2:
-        current_indi.hot = message.text
+        users[str(message.chat.id)].hot = message.text
         cold_keyboard(message)
     elif current_level == 3:
-        current_indi.cold = message.text
+        users[str(message.chat.id)].cold = message.text
         elct_keyboard(message)
     elif current_level == 4:
-        current_indi.elct = message.text
+        users[str(message.chat.id)].elct = message.text
         confirm_keyboard(message)
 # генерация клавиатур
 
